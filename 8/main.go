440b3c14506ba3a0bb8aa57342475804e92d6ea3
Nikita <nikita2054@gmail.com>
2025-09-01 15:03:09 +0300

    l1.8

---
+++ 8/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func setBit(num int64, pos uint) int64 {
+	num |= 1 << pos
+	return num
+}
+
+func clearBit(num int64, pos uint) int64 {
+	num &^= 1 << pos
+	return num
+}
+
+func main() {
+
+	for i := 1; i < 25; i++ {
+
+		fmt.Printf("Исходное число: %d (%b)\n", i, i)
+		newNum := int64(i)
+
+		newNum = setBit(newNum, 1)
+		fmt.Printf("После установки i-го бита в 1: %d (%b)\n", newNum, newNum)
+
+		newNum = clearBit(newNum, 1)
+		fmt.Printf("После установки i-го бита в 0: %d (%b)\n\n", newNum, newNum)
+
+	}
+}

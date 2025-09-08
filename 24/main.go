18bab6c52c4621a30db0528061a48264e814fb69
Nikita <nikita2054@gmail.com>
2025-09-08 14:14:08 +0300

    l1.24

---
+++ 24/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) Point {
+	return Point{x: x, y: y}
+}
+
+func (p Point) Distance(other Point) float64 {
+	cx := p.x - other.x
+	cy := p.y - other.y
+	return math.Sqrt(cx*cx + cy*cy)
+}
+
+func main() {
+
+	p1 := NewPoint(1.0, 2.0)
+	p2 := NewPoint(4.0, 6.0)
+
+	c := p1.Distance(p2)
+
+	fmt.Printf("Расстояние между точками: %.2f\n", c)
+}
